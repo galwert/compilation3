4fd702666679cd370331524392cc57abb8f7cf4e
galwert <wertheizer@campus.technion.ac.il>
2022-12-12 12:11:36 +0200

    compiled

---
+++ Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by galwe on 12/12/2022.
+//
+
+#include "Node.h"
---
+++ Node.h
@@ -0,0 +1,16 @@
+//
+// Created by galwe on 12/12/2022.
+//
+
+#ifndef HW3_NODE_H
+#define HW3_NODE_H
+
+
+
+class Node {
+
+};
+
+
+
+#endif //HW3_NODE_H
